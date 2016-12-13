35a9c43a7a7f73813e41c06b045850d61d7442d0
MrKuBu <mrkubu122333@gmail.com>
2016-12-13 20:22:14 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!-- 
+  ____          __  __      _  __     ____        
+ |  _ \        |  \/  |    | |/ /    |  _ \       
+ | |_) |_   _  | \  / |_ __| ' /_   _| |_) |_   _ 
+ |  _ <| | | | | |\/| | '__|  <| | | |  _ <| | | |
+ | |_) | |_| | | |  | | |  | . \ |_| | |_) | |_| |
+ |____/ \__, | |_|  |_|_|  |_|\_\__,_|____/ \__,_|
+         __/ |                                    
+        |___/            
+		
+
+--><!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>[MrKuBu] Официальный сайт</title> <!-- название сайта в шапке -->
+<link rel="stylesheet" type="text/css" href="assets/css/home.css">
+<script type="text/javascript" src="assets/js/lib/jquery-3.0.0.min.js"></script>
+<script type="text/javascript" src="assets/js/lib/typed.js"></script>
+<link rel="stylesheet" type="text/css" href="assets/css/footer_home.css">
+</head>
+<head>
+ <link rel="shortcut icon" href="/assets/img/icons/favicon.ico" type="image/x-icon">
+</head>
+<body>
+<div id="home"><div>
+<span id="mk">
+<span id="mkt">Добро пожаловать на сайт </span> MrKuBu<br> <!-- Фразу(если вылетит скрпт и ник или какое то название -->
+</span>
+</div></div>
+<div id="footer" class="contentsize">
+<div id="table">
+</ul>
+</td>
+</tr></table>
+</div>
+<body oncontextmenu="return false">
+<div id="arrow"><img src="assets/img/arrow.png"/></div>
+</a>
+<a target="_blank" href="https://vk.com/mrkubu"><img src="assets/img/icons/vk.png" width="30" height="30"/></a> <!-- Ссылка на VK -->
+<a target="_blank" href="https://www.youtube.com/c/mrkubu"><img src="assets/img/icons/youtube.png" width="32" height="32"/></a> <!-- Ссылка на YouTube -->
+<a target="_blank" href="https://steamcommunity.com/id/MrKuBu/"><img src="assets/img/icons/steam.png" width="32" height="32"/></a> <!-- Ссылка на Steam -->
+<a target="_blank" href="https://vk.com/mr.kubu"><img src="assets/img/icons/forum.png" width="32" height="32"/></a> <!-- Ссылка на Форум/обсуждение -->
+<a href="https://facepunch.com/member.php?u=731539" target="_blank">
+<img id="i2" src="assets/img/icons/facepunch.png"/>
+</a>
+<a href="mailto:mrkubu122333@gmail.com" target="_blank">
+<img id="i3" src="assets/img/icons/email.png"/>
+</a>
+
+<!-- !!!Не трогать если не хотите иметь ломаный сайт!!! -->
+</div>
+<div id="footer">
+Copyright &copy; 2016 MrKuBu<br><br>
+</div>
+<!-- !!!Не трогать если не хотите иметь ломаный сайт!!! -->
+<script type="text/javascript">
+        
+            $("#mk > #mkt").typed({
+                strings: ["Добро пожаловать на сайт ","Посетите мой GitHub ","Удачи вам ","Оставайтесь с нами ","Играйте у нас "], <!-- сюда фразы -->
+                typeSpeed: 50,
+                backSpeed: 50,
+                backDelay: 1000,
+                loop: true,
+            });
+            
+            var arrowvisible = true;
+            $(window).scroll(function() {
+                if ($(window).scrollTop() > 0) {
+                    if (arrowvisible) {
+                        arrowvisible = false;
+                        $("#arrow").addClass("invisible");
+                    }
+                } else if (!arrowvisible) {
+                    arrowvisible = true;
+                    $("#arrow").removeClass("invisible");
+                }
+            });
+</script>
+<script type="text/javascript">
+			
+			new Opentip($("#i1"),"GitHub",{style:"dark",target:true,tipJoint:"top"});
+			
+			new Opentip($("#i2"),"Steam",{style:"dark",target:true,tipJoint:"top"});
+
+			new Opentip($("#i3"),"Email",{style:"dark",target:true,tipJoint:"top"});
+			
+</script>
+<script type="text/javascript">/* <![CDATA[ */(function(d,s,a,i,j,r,l,m,t){try{l=d.getElementsByTagName('a');t=d.createElement('textarea');for(i=0;l.length-i;i++){try{a=l[i].href;s=a.indexOf('/cdn-cgi/l/email-protection');m=a.length;if(a&&s>-1&&m>28){j=28+s;s='';if(j<m){r='0x'+a.substr(j,2)|0;for(j+=2;j<m&&a.charAt(j)!='X';j+=2)s+='%'+('0'+('0x'+a.substr(j,2)^r).toString(16)).slice(-2);j++;s=decodeURIComponent(s)+a.substr(j,m-j)}t.innerHTML=s.replace(/</g,'&lt;').replace(/>/g,'&gt;');l[i].href='mailto:'+t.value}}catch(e){}}}catch(e){}})(document);/* ]]> */</script></body>
+</html>
+<!--
+   ____   __   __      __  __     ____      _  __      _   _    ____     _   _  
+U | __")u \ \ / /    U|' \/ '|uU |  _"\ u  |"|/ /   U |"|u| |U | __")uU |"|u| | 
+ \|  _ \/  \ V /     \| |\/| |/ \| |_) |/  | ' /     \| |\| | \|  _ \/ \| |\| | 
+  | |_) | U_|"|_u     | |  | |   |  _ <  U/| . \\u    | |_| |  | |_) |  | |_| | 
+  |____/    |_|       |_|  |_|   |_| \_\   |_|\_\    <<\___/   |____/  <<\___/  
+ _|| \\_.-,//|(_     <<,-,,-.    //   \\_,-,>> \\,-.(__) )(   _|| \\_ (__) )(   
+(__) (__)\_) (__)     (./  \.)  (__)  (__)\.)   (_/     (__) (__) (__)    (__)
+-->
---
+++ mkstat.php
@@ -0,0 +1,71 @@
+<!-- 
+  ____          __  __      _  __     ____        
+ |  _ \        |  \/  |    | |/ /    |  _ \       
+ | |_) |_   _  | \  / |_ __| ' /_   _| |_) |_   _ 
+ |  _ <| | | | | |\/| | '__|  <| | | |  _ <| | | |
+ | |_) | |_| | | |  | | |  | . \ |_| | |_) | |_| |
+ |____/ \__, | |_|  |_|_|  |_|\_\__,_|____/ \__,_|
+         __/ |                                    
+        |___/            
+		
+
+-->
+<!-- CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<title>Чек сайта By MrKuBu</title>
+
+<style>
+	.label-check {
+		display: inline-block;width: 200px
+	}
+</style>
+			<style>
+				body {
+					background: url("http://p1.pichost.me/i/11/1344899.jpg") no-repeat center center fixed;
+					-webkit-background-size: cover;
+					-moz-background-size: cover;
+					-o-background-size: cover;
+					background-size: cover;
+				}
+			</style>
+<div class=
+"modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h1 class="text-center">Проверка дополнений сайта By MrKuBu</h1>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<center><?php
+$source = ini_get_all();
+
+$check_list = array(
+	'sourceguardian'=>'Sourceguardian',
+	'mysqli'=>'MySQLi',
+	'mbstring'=>'Mbstring',
+	'bcmath'=>'bcmath',
+	'bz2'=>'bz2',
+	'sockets'=>'Sockets',
+	'simplexml'=>'SimpleXML',
+	'openssl'=>'OpenSSL',
+	'json'=>'JSON',
+	'xml'=>'Xml',
+);
+
+foreach ($check_list as $name => $label) {
+	echo "<div style='margin-bottom: 5px'><span class='label-check'>$label: </span>" . (extension_loaded($name) ? "<span class='btn btn-success btn-sm'>Есть\работает</span>" : '<span class="btn btn-danger btn-sm">Нету\Отключено</span>') . '</div>';
+}
+
+echo "<div style='margin-bottom: 5px'><span class='label-check'>allow_url_fopen: </span>" . ((ini_get('allow_url_fopen')) ? "<span class='btn btn-success btn-sm'>Есть\работает</span>" : '<span class="btn btn-danger btn-sm">Нету\Отключено</span>') . '</div>';
+?></center>
+
+				</div>
+			</div>
+		</div>
